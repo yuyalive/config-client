332b0655e5893a32068d4c2150e93ad3b2d18f44
yuyalive <yuya.hirose0725@gmail.com>
2018-04-10 00:37:00 +0900

    first commit

---
+++ src/main/java/sample/service/SamplePropController.java
@@ -0,0 +1,17 @@
+package sample.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class SamplePropController {
+
+    @RequestMapping("/foo")
+    public String getFoo(@Value("${foo:notfound}") String foo) { return foo; }
+
+    @RequestMapping("/bar")
+    public String getBar(@Value("${bar:notfound}") String bar) { return bar; }
+}
